9facca2c936df6b14b6bc6162389ace294abc271
sumit9090 <sumit.kalra121@gmail.com>
2024-04-06 14:02:31 +0530

    Add files via upload

---
+++ MavenProjectFromEclipse/src/test/java/com/qtpselenium/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.qtpselenium;
+
+import org.testng.annotations.Test;
+
+@Test
+public class HelloWorld
+
+{
+	@Test
+	public void Test1()
+	{
+		System.out.println("hello world");;
+	}
+}
+
